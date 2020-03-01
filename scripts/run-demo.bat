9a62d945272de9060b4cb8874203b1f99e45dd79
Martino Nikolovski <nikolovski23@gmail.com>
2020-03-01 01:10:54 -0800

    modified run-demo.bat

--- scripts/run-demo.bat
+++ scripts/run-demo.bat
@@ -1,5 +1,4 @@
 ECHO Starting the backend
-export dbPassword=test
 start "Remitly Shortly BE" /D "../remitly-shortly-be" mvn spring-boot:run
 ECHO Starting the frontend
 start "Remitly Shortly FE" /D "../remitly-shortly-ui" ng serve
